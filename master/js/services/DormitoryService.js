ff88f76f50c482652a2d73b8cb85fecd225bb8cc
sx-9023 <sx-wangx@dtdream.com>
2015-07-24 17:23:11 +0800

    表格就那样了，决定把导入导出全部交给服务器做，今晚把json的格式商定好，然后把所有表格的表头确定下来，跟服务器进行简单的对接

---
+++ master/js/services/DormitoryService.js
@@ -0,0 +1,33 @@
+App.service('DormitoryService', function() {
+
+  var TableData = {
+    cache: null,
+    getData: function($defer, params, api){
+      // if no cache, request data and filter
+      if ( ! TableData.cache ) {
+        if ( api ) {
+          api.get(function(data){
+            TableData.cache = data;
+            filterdata($defer, params);
+          });
+        }
+      }
+      else {
+        filterdata($defer, params);
+      }
+      
+      function filterdata($defer, params) {
+        var from = (params.page() - 1) * params.count();
+        var to = params.page() * params.count();
+        var filteredData = TableData.cache.result.slice(from, to);
+
+        params.total(TableData.cache.total);
+        $defer.resolve(filteredData);
+      }
+
+    }
+  };
+  
+  return TableData;
+
+});
